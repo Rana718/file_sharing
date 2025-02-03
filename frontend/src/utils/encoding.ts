e33cfc5303c748c0372f1c3252e08ecb99b8d6f8
Rana718 <ranadolui718@gmail.com>
2025-02-03 07:58:01 +0530

    updateing the encoding

--- frontend/src/utils/encoding.ts
+++ frontend/src/utils/encoding.ts
@@ -1,16 +1,14 @@
-
-
-const arrayBufferToBase64 = (buffer: any) => {
+const arrayBufferToBase64 = (buffer: ArrayBuffer): string => {
     let binary = '';
     const bytes = new Uint8Array(buffer);
     for (let i = 0; i < bytes.byteLength; i++) {
         binary += String.fromCharCode(bytes[i]);
     }
-
     return btoa(binary).replace(/\+/g, '-').replace(/\//g, '_');
-}
+};
 
-const base64ToUint8Array = (base64:any) => {
+
+const base64ToUint8Array = (base64: string): Uint8Array => {
     const fixedBase64 = base64.replace(/-/g, '+').replace(/_/g, '/');
     const paddedBase64 = fixedBase64.padEnd(fixedBase64.length + (4 - fixedBase64.length % 4) % 4, '=');
     const binary = atob(paddedBase64);
@@ -20,6 +18,5 @@ const base64ToUint8Array = (base64:any) => {
     }
     return bytes;
 };
-
 
 export { arrayBufferToBase64, base64ToUint8Array };
